93eadae354abc4d9a6d4c983d395d3a83e65f3ff
14justin <justinmwangi40@gmail.com>
2024-03-19 21:13:54 +0300

    test

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+    if(value <= 400){
+    return "This one is on me!"
+  } else if (value >399 && value < 2000){
+    return 'That will be twenty bucks.'
+  }
+    else if(2000<value && value<=2500){
+      return 'I will gladly take your thirty bucks.'
+   }
+    else
+     return "No can do."
+     
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city=='NYC'){
+    return "Ok, sounds good."
+  }
+   else
+   return"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+     case 'not as generous':
+       return 'Thank you.';
+      default:
+        return 'Bye.';  
+  }
 }
